c1ed24b63e095a3edb6d5a3471e212aeea529e2f
RemigiuszDrobinski <curiosisciekawski@gmail.com>
2020-11-09 10:35:49 +0100

    Upgrade creatingBoard fun

--- ChessMaster/Assets/Scripts/Board.cs
+++ ChessMaster/Assets/Scripts/Board.cs
@@ -6,13 +6,13 @@ using UnityEngine.UI;
 public class Board : MonoBehaviour
 {
     public GameObject mCellPrefab;
-    public Cell[,] mAllCells = new Cell[12, 12];
+    public Cell[,] mAllCells = new Cell[8, 8];
 
     public void Create()
     {
-        for(int i=0; i<12; i++)
+        for(int i=0; i<8; i++)
         {
-            for (int j = 0; j < 12; j++)
+            for (int j = 0; j < 8; j++)
             {
                 GameObject newCell = Instantiate(mCellPrefab, transform);
                 RectTransform rectTransform = newCell.GetComponent<RectTransform>();
@@ -22,9 +22,9 @@ public class Board : MonoBehaviour
             }
         }
 
-        for(int i=0; i<12; i += 2)
+        for(int i=0; i<8; i += 2)
         {
-            for(int j=0; j<12; j++)
+            for(int j=0; j<8; j++)
             {
                 int offset = (j % 2 != 0) ? 0 : 1;
                 int finalX = i + offset;
