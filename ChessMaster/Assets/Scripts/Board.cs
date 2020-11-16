c6a65558c5f91ba471e22f7890fd6f55538570ad
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2020-11-16 17:50:03 +0100

    Add color board

--- ChessMaster/Assets/Scripts/Board.cs
+++ ChessMaster/Assets/Scripts/Board.cs
@@ -5,31 +5,32 @@ using UnityEngine.UI;
 
 public class Board : MonoBehaviour
 {
+    public static int boardSize = 8;
     public GameObject mCellPrefab;
-    public Cell[,] mAllCells = new Cell[8, 8];
+    public Cell[,] mAllCells = new Cell[boardSize, boardSize];
 
     public void Create()
     {
-        for(int i=0; i<8; i++)
+        for(int i=0; i< boardSize; i++)
         {
-            for (int j = 0; j < 8; j++)
+            for (int j = 0; j < boardSize; j++)
             {
                 GameObject newCell = Instantiate(mCellPrefab, transform);
                 RectTransform rectTransform = newCell.GetComponent<RectTransform>();
                 rectTransform.anchoredPosition = new Vector2((i * 100) + 50, (j * 100) + 50);
                 mAllCells[i, j] = newCell.GetComponent<Cell>();
                 mAllCells[i, j].Setup(new Vector2Int(i, j), this);
+                mAllCells[i, j].GetComponent<Image>().color = new Color32(83, 62, 45, 255);
             }
         }
-
-        for(int i=0; i<8; i += 2)
+        
+        for (int i=0; i< boardSize; i += 2)
         {
-            for(int j=0; j<8; j++)
+            for(int j=0; j< boardSize; j++)
             {
                 int offset = (j % 2 != 0) ? 0 : 1;
                 int finalX = i + offset;
-
-                mAllCells[finalX, j].GetComponent<Image>().color = new Color32(230, 220, 187, 255);
+                mAllCells[finalX, j].GetComponent<Image>().color = new Color32(221, 202, 125, 255);
             }
         }
     }
